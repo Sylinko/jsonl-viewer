fa19f4fafa3f2fe8ba34e1fb333537f16dc91aca
Artemis Li <auroraziling@gmail.com>
2026-01-15 01:33:45 +0800

    Revert "cloudflare compability"
    This reverts commit dac6c2d0fd8ebb41adf0859e3a65b417f6358984.

--- next.config.js
+++ next.config.js
@@ -2,8 +2,7 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-  output: 'standalone',
+  disable: process.env.NODE_ENV === 'development'
 });
 
 /** @type {import('next').NextConfig} */
