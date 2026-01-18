d450761fa82f1190640d0ab159a29e0b29b798f6
Artemis Li <auroraziling@gmail.com>
2026-01-18 17:10:58 +0800

    add PWA support

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development'
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: false, // Enable in dev for testing
+  workboxOptions: {
+    disableDevLogs: true,
+  },
 });
 
 /** @type {import('next').NextConfig} */
